5cbb59b58c24f433488312f31247f41aef777311
Guilherme Lellis <lguilherme44@gmail.com>
2021-03-05 23:45:50 -0300

    refatorando

--- frontend/src/pages/EditarUsuario/index.js
+++ frontend/src/pages/EditarUsuario/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // font-awesome
 import { FaSpinner, FaSave } from 'react-icons/fa';
@@ -13,139 +13,100 @@ import 'react-toastify/dist/ReactToastify.css';
 // chamada da api
 import api from '../../services/api';
 
-// components
-// import Container from '../../components/Container';
-
 // styled-components
 import { Form, SubmitButton, List } from './styles';
 import ContainerStyle from '../../components/Container';
 import Header from '../../components/Header';
 
-export default class Main extends Component {
-  constructor() {
-    super();
+export default function EditarUsuario({ match }) {
+  const [loading, setLoading] = useState(false);
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [usuario, setUsuario] = useState({});
 
-    this.handleValuesChange.bind(this);
-  }
-  state = {
-    cnpj: '',
-    loading: false,
-    data: {},
-    usuario: {},
-  };
+  useEffect(() => {
+    async function getUser() {
+      let { data, status, ok } = await api.get(`/usuario/${match.params.id}`);
 
-  async getUser(id) {
-    try {
-      let { data } = await api.get(`/usuario/${id}`);
-
-      this.setState({ usuario: data });
-    } catch (error) {
-      // toast.error(
-      //   error.response.data ? error.response.data.error : error.message
-      // );
-    }
-  }
-
-  async componentDidMount() {
-    const id = this.props.match.params.id;
-    this.getUser(id);
-  }
-
-  handleInputChange = e => {
-    this.setState({ cnpj: e.target.value });
-  };
-
-  handleValuesChange = e => {
-    this.setState({
-      usuario: {
-        ...this.state.usuario,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
-
-  handleSubmitApi = async e => {
-    e.preventDefault();
-    try {
-      const dados = this.state.usuario;
-
-      //console.log(dados);
-
-      this.setState({ loading: true });
-
-      await api.put(`/usuario/${dados.id}`, {
-        nome: dados.nome,
-        email: dados.email,
-      });
-
-      // se sucesso
-      toast.success('Cadastro realizado com sucesso!');
-
-      // redireciona se sucesso para a home
-    } catch (erro) {
-      // se erro
-      if (erro.response) {
-        // response status, data
-        let { error } = erro.response.data;
-        toast.error(error);
+      if (status === 200 && ok) {
+        setNome(data.nome);
+        setEmail(data.email);
+        setUsuario(data);
       }
     }
 
-    this.setState({ loading: false });
+    getUser();
+  }, [match]);
+
+  const handleSubmitApi = async e => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const { status, ok, problem } = await api.put(`/usuario/${usuario.id}`, {
+      nome,
+      email,
+    });
+
+    console.log(problem);
+
+    if (status === 200 && ok) {
+      toast.success('Cadastro atualizado com sucesso!');
+    } else {
+      toast.error('Problema ao carregar dados da API');
+    }
+
+    setLoading(false);
   };
 
-  render() {
-    const { loading, usuario } = this.state;
+  return (
+    <>
+      <Header />
+      <List>
+        <Form onSubmit={handleSubmitApi}>
+          <Container>
+            <Row>
+              <Col>
+                <h3 className="text-muted">Editar Usuário</h3>
+              </Col>
+            </Row>
 
-    return (
-      <>
-        <Header />
-        <List>
-          <Form onSubmit={this.handleSubmitApi}>
-            <Container>
+            <ContainerStyle>
               <Row>
-                <Col>
-                  <h3 className="text-muted">Editar Usuário</h3>
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label className="input-w">Nome:</label>
+                  <input
+                    className="label"
+                    type="text"
+                    value={nome}
+                    name="nome"
+                    onChange={e => setNome(e.target.value)}
+                  />
+                </Col>
+
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label className="input-w">E-mail:</label>
+                  <input
+                    className="label"
+                    type="text"
+                    value={email}
+                    name="email"
+                    onChange={e => setEmail(e.target.value)}
+                  />
                 </Col>
               </Row>
 
-              <ContainerStyle>
-                <Row>
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label className="input-w">Nome:</label>
-                    <input
-                      className="label"
-                      type="text"
-                      value={usuario.nome}
-                      name="nome"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
-
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label className="input-w">E-mail:</label>
-                    <input
-                      className="label"
-                      type="text"
-                      value={usuario.email}
-                      name="email"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
-                </Row>
-
-                <SubmitButton loading={loading}>
-                  {loading ? (
-                    <FaSpinner color="#FFF" size={14} />
-                  ) : (
-                    <FaSave color="#FFF" size={14} />
-                  )}
-                </SubmitButton>
-              </ContainerStyle>
-            </Container>
-          </Form>
-        </List>
-      </>
-    );
-  }
+              <SubmitButton loading={loading}>
+                {loading ? (
+                  <FaSpinner color="#FFF" size={14} />
+                ) : (
+                  <FaSave color="#FFF" size={14} />
+                )}
+              </SubmitButton>
+            </ContainerStyle>
+          </Container>
+        </Form>
+      </List>
+    </>
+  );
 }
