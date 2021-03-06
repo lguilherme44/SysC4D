b0bbdea1dd57280cd0c2f912202e2f378dd51d59
Guilherme Lellis <lguilherme44@gmail.com>
2021-03-05 23:47:43 -0300

    migração para mysql

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,10 @@
 module.exports = {
-  dialect: 'sqlite',
-  host: 'localhost',
-  // username: 'postgres',
-  // password: 'aaa123456@',
-  storage: 'db.sqlite',
+  dialect: 'mysql',
+  host: '192.168.0.251',
+  username: 'sysc4d',
+  password: '123456',
+  // storage: 'db.sqlite',
+  database: 'sysc4d',
   define: {
     timestamps: true,
     underscored: true,
