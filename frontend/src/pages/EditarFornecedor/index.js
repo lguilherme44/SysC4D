865ed7a088cdb8089da670fe7043b09b401ab009
Guilherme Lellis <lguilherme44@gmail.com>
2021-03-06 00:17:20 -0300

    refatorando

--- frontend/src/pages/EditarFornecedor/index.js
+++ frontend/src/pages/EditarFornecedor/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // font-awesome
 import { FaSpinner, FaSave } from 'react-icons/fa';
@@ -21,197 +21,175 @@ import { Form, SubmitButton, List } from './styles';
 import ContainerStyle from '../../components/Container';
 import Header from '../../components/Header';
 
-export default class Main extends Component {
-  constructor() {
-    super();
+export default function EditarFornecedor({ match }) {
+  const [cnpj, setCnpj] = useState('');
+  const [nome, setNome] = useState('');
+  const [situacao, setSituacao] = useState('');
+  const [logradouro, setLogradouro] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [numero, setNumero] = useState('');
+  const [cep, setCep] = useState('');
+  const [municipio, setMunicipio] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    this.handleValuesChange.bind(this);
-  }
-  state = {
-    cnpj: '',
-    loading: false,
-    data: {},
-    fornecedor: {},
-  };
+  useEffect(() => {
+    async function getForn() {
+      const { data, status, ok, problem } = await api.get(
+        `/fornecedor/${match.params.id}`
+      );
 
-  async getForn(id) {
-    try {
-      console.log(id);
-      let { data } = await api.get(`/fornecedor/${id}`);
-
-      this.setState({ fornecedor: data });
-    } catch (error) {
-      // toast.error(
-      //   error.response.data ? error.response.data.error : error.message
-      // );
-    }
-  }
-
-  async componentDidMount() {
-    const id = this.props.match.params.id;
-    this.getForn(id);
-  }
-
-  handleInputChange = e => {
-    this.setState({ cnpj: e.target.value });
-  };
-
-  handleValuesChange = e => {
-    this.setState({
-      fornecedor: {
-        ...this.state.fornecedor,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
-
-  handleSubmitApi = async e => {
-    e.preventDefault();
-    try {
-      const dados = this.state.fornecedor;
-
-      //console.log(dados);
-
-      this.setState({ loading: true });
-
-      await api.put(`/fornecedor/${dados.id}`, {
-        cnpj: dados.cnpj,
-        nome: dados.nome,
-        situacao: dados.situacao,
-        logradouro: dados.logradouro,
-        bairro: dados.bairro,
-        numero: dados.numero,
-        cep: dados.cep,
-        municipio: dados.municipio,
-      });
-
-      // se sucesso
-      toast.success('Cadastro realizado com sucesso!');
-
-      // redireciona se sucesso para a home
-    } catch (erro) {
-      // se erro
-      if (erro.response) {
-        // response status, data
-        let { error } = erro.response.data;
-        toast.error(error);
+      if (status === 200 && ok) {
+        setCnpj(data.cnpj);
+        setNome(data.nome);
+        setSituacao(data.situacao);
+        setLogradouro(data.logradouro);
+        setBairro(data.bairro);
+        setNumero(data.numero);
+        setCep(data.cep);
+        setMunicipio(data.municipio);
+      } else {
+        toast.error(problem);
       }
     }
 
-    this.setState({ loading: false });
+    getForn();
+  }, [match]);
+
+  const handleSubmitApi = async e => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const { status, ok, problem } = await api.put(
+      `/fornecedor/${match.params.id}`,
+      {
+        cnpj,
+        nome,
+        situacao,
+        logradouro,
+        bairro,
+        numero,
+        cep,
+        municipio,
+      }
+    );
+
+    if (status === 200 && ok) {
+      toast.success('Cadastro atualizado com sucesso!');
+    } else {
+      toast.error(problem);
+    }
+
+    setLoading(false);
   };
 
-  render() {
-    const { loading, fornecedor } = this.state;
-
-    return (
-      <>
-        <Header />
-        <List>
-          <Form onSubmit={this.handleSubmitApi}>
-            <Container>
+  return (
+    <>
+      <Header />
+      <List>
+        <Form onSubmit={handleSubmitApi}>
+          <Container>
+            <Row>
+              <Col>
+                <h3 className="text-muted">Editar Fornecedor</h3>
+              </Col>
+            </Row>
+            <ContainerStyle>
               <Row>
-                <Col>
-                  <h3 className="text-muted">Editar Fornecedor</h3>
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label className="input-w">CNPJ:</label>
+                  <input
+                    className="label"
+                    type="text"
+                    value={cnpj}
+                    name="cnpj"
+                    onChange={e => setCnpj(e.target.value)}
+                  />
+                </Col>
+
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label className="input-w">Razão:</label>
+                  <input
+                    className="label"
+                    type="text"
+                    value={nome}
+                    name="nome"
+                    onChange={e => setNome(e.target.value)}
+                  />
+                </Col>
+
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label>Situação:</label>
+                  <input
+                    type="text"
+                    value={situacao}
+                    name="situacao"
+                    onChange={e => setSituacao(e.target.value)}
+                  />
+                </Col>
+
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label>Logradouro:</label>
+                  <input
+                    type="text"
+                    value={logradouro}
+                    name="logradouro"
+                    onChange={e => setLogradouro(e.target.value)}
+                  />
+                </Col>
+
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label>Bairro:</label>
+                  <input
+                    type="text"
+                    value={bairro}
+                    name="bairro"
+                    onChange={e => setBairro(e.target.value)}
+                  />
+                </Col>
+
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label>Numero:</label>
+                  <input
+                    type="text"
+                    value={numero}
+                    name="numero"
+                    onChange={e => setNumero(e.target.value)}
+                  />
+                </Col>
+
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label>Cep:</label>
+                  <input
+                    type="text"
+                    value={cep}
+                    name="cep"
+                    onChange={e => setCep(e.target.value)}
+                  />
+                </Col>
+
+                <Col xs="12" sm="12" md="12" lg="6">
+                  <label>Municipio:</label>
+                  <input
+                    type="text"
+                    value={municipio}
+                    name="municipio"
+                    onChange={e => setMunicipio(e.target.value)}
+                  />
                 </Col>
               </Row>
-              <ContainerStyle>
-                <Row>
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label className="input-w">CNPJ:</label>
-                    <input
-                      className="label"
-                      type="text"
-                      value={fornecedor.cnpj}
-                      name="cnpj"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
 
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label className="input-w">Razão:</label>
-                    <input
-                      className="label"
-                      type="text"
-                      value={fornecedor.nome}
-                      name="nome"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
-
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label>Situação:</label>
-                    <input
-                      type="text"
-                      value={fornecedor.situacao}
-                      name="situacao"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
-
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label>Logradouro:</label>
-                    <input
-                      type="text"
-                      value={fornecedor.logradouro}
-                      name="logradouro"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
-
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label>Bairro:</label>
-                    <input
-                      type="text"
-                      value={fornecedor.bairro}
-                      name="bairro"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
-
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label>Numero:</label>
-                    <input
-                      type="text"
-                      value={fornecedor.numero}
-                      name="numero"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
-
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label>Cep:</label>
-                    <input
-                      type="text"
-                      value={fornecedor.cep}
-                      name="cep"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
-
-                  <Col xs="12" sm="12" md="12" lg="6">
-                    <label>Municipio:</label>
-                    <input
-                      type="text"
-                      value={fornecedor.municipio}
-                      name="municipio"
-                      onChange={this.handleValuesChange}
-                    />
-                  </Col>
-                </Row>
-
-                <SubmitButton loading={loading}>
-                  {loading ? (
-                    <FaSpinner color="#FFF" size={14} />
-                  ) : (
-                    <FaSave color="#FFF" size={14} />
-                  )}
-                </SubmitButton>
-              </ContainerStyle>
-            </Container>
-          </Form>
-        </List>
-      </>
-    );
-  }
+              <SubmitButton loading={loading}>
+                {loading ? (
+                  <FaSpinner color="#FFF" size={14} />
+                ) : (
+                  <FaSave color="#FFF" size={14} />
+                )}
+              </SubmitButton>
+            </ContainerStyle>
+          </Container>
+        </Form>
+      </List>
+    </>
+  );
 }
